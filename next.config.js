47a7d5684cb420bd28219f6f1b1e18d0262cf3ef
LammieJ <118027040+LammieJ@users.noreply.github.com>
2025-02-16 00:06:49 +0000

    Update next.config.js to handle WebSocket dependencies

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.externals.push({
+      'utf-8-validate': 'commonjs utf-8-validate',
+      'bufferutil': 'commonjs bufferutil',
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
